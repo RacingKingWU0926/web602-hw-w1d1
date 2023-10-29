c1454f68c63ed5d68a24182f1e7004cf184331fd
JasonLei <jlei@activision.com>
2023-10-29 13:10:30 -0700

    completed task2

--- server_task1.js
+++ server_task1.js
@@ -8,17 +8,17 @@ const port = 5000
 
 // create the web server
 var server = http.createServer((req, res) => {
-    const filePath = req.url;
+    const fileUrl = req.url;
 
-    if (filePath === '/') {
+    if (fileUrl === '/') {
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/html');
         res.end(`<html><body><h1>Home Page</h1></body></html>`);
-    } else if (filePath === '/about') {
+    } else if (fileUrl === '/about') {
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/html');
         res.end(`<html><body><h1>About Page</h1></body></html>`);
-    } else if (filePath === '/contact') {
+    } else if (fileUrl === '/contact') {
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/html');
         res.end(`<html><body><h1>Contact Page</h1></body></html>`);
